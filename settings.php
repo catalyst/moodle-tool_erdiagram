ce9c9d67af84cdecbfdfa40f79d84f9dcd39a367
Brendan Heywood <brendan@catalyst-au.net>
2023-11-22 12:10:27 +1100

    Add link to developer admin menu #5

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,20 +12,18 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Plugin strings are defined here.
+ * Link to ER diagram
  *
- * @package     tool_erdiagram
- * @category    string
- * @author      Marcus Green
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    tool_erdiagram
+ * @copyright  2003 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die;
 
-$string['pluginname'] = 'DB Entity Relationship Diagrams';
-$string['pluginfolder']   = 'Plugin folder';
-$string['pluginfolder_help'] = 'Path to single plugin e.g. mod/label';
-
+if ($hassiteconfig) {
+    $ADMIN->add('development', new admin_externalpage('toolerdiagram', get_string('pluginname', 'tool_erdiagram'), "$CFG->wwwroot/$CFG->admin/tool/erdiagram/"));
+}
